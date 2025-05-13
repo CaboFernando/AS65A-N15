be7172c6a2ec0713fd97f782deb52a08af12f2ed
AndreSouza94 <andre_souzaprof@hotmail.com>
2025-05-13 17:23:27 -0300

    Add files via upload

--- js/login.js
+++ js/login.js
@@ -1,19 +1,35 @@
 document.getElementById("login-form").addEventListener("submit", function(e) {
     e.preventDefault();
-    const cpf = document.getElementById("cpf").value || null;
-    const email = document.getElementById("email").value;
+    
+    const login = document.getElementById("login").value.trim();
     const senha = document.getElementById("senha").value;
+
+    if(!login || !senha){
+        alert("Por favor, preencha todos os campos")
+        return;
+    }
+
+    let payload = {senha: senha};
+
+    const cpfLimpo = login.replace(/\D/g, "");
+    if (cpfLimpo.length === 11){   // CPF tem que ter 11 digitos
+        payload.cpf = cpfLimpo;
+    } 
+    else{
+        const emailValido = /^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(login);
+        if (!emailValido) {
+            alert("Digite um CPF válido (11 números) ou um e-mail válido.");
+            return;
+        }
+        payload.email = login;
+    }
 
     fetch('api/login' ,{
         method: 'POST',
         headers: {
             'Content-Type': 'application/json'
         },
-        body: JSON.stringify({
-            cpf: cpf,
-            email: email,
-            senha: senha
-        })
+        body: JSON.stringify({payload})
     })
     .then(res => {
         if(res.ok) return window.location.href = "home.html";
